066fa304f3084dc08cb50db94062ed17895fa7e2
d.yurchyliak <dima7685@gmail.com>
2026-03-05 13:27:24 +0200

    Refresh token

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,71 +1,37 @@
-using archolosDotNet.Models.Extensions;
-using archolosDotNet.Models.Pagination;
-using archolosDotNet.Models.Payload;
 using archolosDotNet.Models.UserNS;
 using archolosDotNet.Services.UserNS;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Npgsql;
 
 namespace archolosDotNet.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController(IAuthService service, TokenProvider tokenProvider) : ControllerBase
+    public class AuthController(IAuthService authService) : ControllerBase
     {
-        private readonly IAuthService authService = service;
-
-        [HttpGet]
-        public PagedResult<SimpleUser> GetAll([FromBody] ListPayload<UserFilter> data)
+        [HttpPost("login")]
+        public ActionResult Login(LoginRequest data)
         {
-            return authService.GetAll().toPagedResult(data.pagination);
-        }
+            var result = authService.Login(data);
 
-        [HttpPost("create")]
-        public ActionResult<UserDto> createUser(UserDto user)
-        {
-            try
+            if (result == null)
             {
-                return Ok(authService.CreateUser(user));
-            }
-            catch (Exception e)
-            {
-                if (e.InnerException is PostgresException npgex && npgex.SqlState == PostgresErrorCodes.UniqueViolation)
-                {
-                    return Conflict("User with this email already exist");
-                }
-
-                return UnprocessableEntity(new { message = "Invalid data", originalError = e });
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var result = authService.Delete(id);
-
-            return result ? NoContent() : NotFound();
-        }
-
-        [HttpGet("token")]
-        [Authorize]
-        public PagedResult<SimpleUser> GetAll2([FromBody] ListPayload<UserFilter> data)
-        {
-            return authService.GetAll().toPagedResult(data.pagination);
-        }
-
-        [HttpPost("token")]
-        public ActionResult<string> GenerateToken(UserDto data)
-        {
-            if(!authService.IsAuthenticated(data))
-            {
-                return Unauthorized();
+                return NotFound("Wrong email or password");
             }
 
-            // var token = jwtService.GenerateToken(data);
-            var token = tokenProvider.Create(data);
+            return Ok(result);
+        }
 
-            return Ok(token);
+        [HttpPost("refresh-token")]
+        public ActionResult LoginWithToken([FromQuery] string token)
+        {
+            var result = authService.RefreshTokens(token);
+
+            if (result == null)
+            {
+                return NotFound("Refresh token has been expired");
+            }
+
+            return Ok(result);
         }
     }
 }
